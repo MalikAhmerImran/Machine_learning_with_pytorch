1a64b612294b56fd7be14842e623afb346266626
MalikAhmerImran <ahmermalik655@gmail.com>
2025-02-25 16:57:32 +0500

    splitting the dataset into training and tesing samples method implemented in utils.py.

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler 
-from utils import standardize_features
+from utils import standardize_features,training_testing_split
+
 data_set=pd.read_csv('datasets/IRIS.csv',encoding='utf-8')
 X=data_set.iloc[:,[2,3]].to_numpy()
 y=data_set.iloc[:,[4]]
-
 y=np.where(y.to_numpy().ravel()=='Iris-setosa',0,np.where(y.to_numpy().ravel()=='Iris-versicolor',1,2))
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
+
+X_train,X_test,y_train,y_test=training_testing_split(X,y)
+
 X_train_std,X_test_std=standardize_features(x_train=X_train,x_test=X_test)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 # utility function to standardize the training and testing the samples
 def standardize_features(x_train:np.ndarray,x_test:np.ndarray):
@@ -7,4 +8,9 @@ def standardize_features(x_train:np.ndarray,x_test:np.ndarray):
     ss.fit(x_train)
     return ss.transform(x_train),ss.transform(x_test)
 
-    
+
+# utility function to split the dataset into training and testing 
+
+def training_testing_split(x_samples:np.ndarray,y_samples:np.ndarray):
+    X_train,X_test,y_train,y_test=train_test_split(x_samples,y_samples,test_size=0.3,random_state=1,stratify=y_samples)
+    return  X_train,X_test,y_train,y_test
