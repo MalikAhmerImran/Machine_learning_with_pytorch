429de2ac70daf73a0683ebfe97c1d0867ef96bbc
MalikAhmerImran <bilal.munirr@gmail.com>
2025-02-25 15:27:54 +0500

    changes updated

--- perceptron.py
+++ perceptron.py
@@ -5,4 +5,4 @@ data_set=pd.read_csv('datasets/IRIS.csv',encoding='utf-8')
 X=data_set.iloc[:,[2,3]].to_numpy()
 y=data_set.iloc[:,[4]]
 y=np.where(y.to_numpy().ravel()=='Iris-setosa',0,np.where(y.to_numpy().ravel()=='Iris-versicolor',1,2))
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1,stratify=  y)
