a6f1776d1ba0a9dafc067f463ae60ead042d9160
MalikAhmerImran <bilal.munirr@gmail.com>
2025-02-25 15:11:00 +0500

    perceptron module created

---
+++ perceptron.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+data_set=pd.read_csv('datasets/IRIS.csv',encoding='utf-8')
+X=data_set.iloc[:,[2,3]].to_numpy()
+y=data_set.iloc[:,[4]]
+y=np.where(y.to_numpy().ravel()=='Iris-setosa',0,np.where(y.to_numpy().ravel()=='Iris-versicolor',1,2))
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
