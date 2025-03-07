bc36cb42b9bd4131dac06b318c2592ff78123db3
MalikAhmerImran <ahmermalik655@gmail.com>
2025-03-07 14:58:57 +0500

    Multilayer Perceptron model class implemented for training.

--- multi_lay_percep.py
+++ multi_lay_percep.py
@@ -1,8 +1,9 @@
+import torch
 import numpy as np
 from sklearn.datasets import load_iris
-from torch.utils.data import TensorDataset
+from torch.utils.data import TensorDataset,DataLoader
 from utils import training_testing_split,standardization
-
+from torch import nn
 
 # Loading the data set from Sklear 
 iris=load_iris()
@@ -13,4 +14,30 @@ y=iris['target']
 X_train,X_test,y_train,y_test=training_testing_split(x_samples=X,y_samples=y)
 
 X_train_norm=standardization(X_train)
-X_test_norm=standardization(X_test)
+X_train_norm=torch.from_numpy(X_train_norm)
+y_train=torch.from_numpy(y_train)
+train_ds=TensorDataset(X_train_norm,y_train)
+torch.manual_seed(1)
+train_dl=DataLoader(train_ds,batch_size=2,shuffle=True)
+
+
+# Creating the neural network for traing the iris data set on multilayer perceptron model
+class Model(nn.Module):
+    def __init__(self,input_size,hidden_size,output_size):
+        super().__init__()
+        self.Layer1=nn.Linear(input_size,hidden_size)
+        self.Layer2=nn.Linear(hidden_size,output_size)
+
+    def forward(self,x):
+        x=self.Layer1(x)
+        x=nn.Sigmoid()(x)
+        x=self.Layer2(x)
+        x=nn.Softmax(dim=1)(x)
+        return x
+    
+model=Model(input_size=X_train_norm.shape[1],hidden_size=16,output_size=3)
+loss_fn=nn.CrossEntropyLoss()
+optimizer=torch.optim.Adam(model.parameters(),lr=0.001)
+num_epochs = 100
+loss_hist = [0] * num_epochs
+accuracy_hist = [0] * num_epochs
