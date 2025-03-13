ce5cd342d5b2e146cedd3b54c10136a6fab8c076
MalikAhmerImran <ahmermalik655@gmail.com>
2025-03-13 11:31:59 +0500

    Dataloader created of train and valid data.

--- cnn.py
+++ cnn.py
@@ -1,14 +1,16 @@
 import torch
 import torchvision
 from torchvision import transforms
-from torch.utils.data import Subset
+from torch.utils.data import Subset ,DataLoader
 
-
+torch.manual_seed(1)
 transforms=transforms.Compose([transforms.ToTensor()])
 path='./'
 mnist_dataset=torchvision.datasets.MNIST(root=path,train=True,transform=transforms,download=True)
 
 valid_mnist_dataset=Subset(mnist_dataset,torch.arange(10000))
-train_mnist_datset=Subset(mnist_dataset,torch.arange(10000,len(mnist_dataset)))
+train_mnist_dataset=Subset(mnist_dataset,torch.arange(10000,len(mnist_dataset)))
 
 test_mnist_data=torchvision.datasets.MNIST(root=path,train=False,transform=transforms,download=True)
+valid_dl=DataLoader(valid_mnist_dataset,batch_size=64,shuffle=False)
+train_dl=DataLoader(train_mnist_dataset,batch_size=64,shuffle=True)
